7ee4750344cb43193b1029a2e9f8761f99e152f0
DVD-R-2 <91427703+DVD-R-2@users.noreply.github.com>
2022-09-26 22:23:38 +0300

    Updates: - am schimbat link-urile de contact in butoane - am terminat versiunea demo la lista card-urilor - am bagat animatii si am facut-o responsive + un script de auto scroll - restul paginilor au primit bugfix-uri - am terminat de reorganizat scss-ul

--- js/card.js
+++ js/card.js
@@ -1,3 +1,4 @@
+let cardLists = document.querySelectorAll('.card-list');
 let cards = document.querySelectorAll('.card');
 let cardElements = Array.from(cards);
 
@@ -8,5 +9,26 @@ cardElements.forEach(card => {
     });
     card.addEventListener("mouseout", () => {
         header.classList.remove("card-header--hover");      
+    });
+    card.addEventListener("click", () => {
+        
+        let cardLeft = card.getBoundingClientRect().left;
+        let cardRight = card.getBoundingClientRect().right;
+        let screenWidth = screen.width;
+        let cardRightA = screenWidth - cardRight;
+        setTimeout(() => {
+            if (cardLeft !== cardRightA) {
+                cardLeft = card.getBoundingClientRect().left;
+                cardRight = card.getBoundingClientRect().right;
+                screenWidth = screen.width;
+                cardRightA = screenWidth - cardRight;
+                let mid = (cardLeft  + cardRightA) / 2;
+                let offSet = cardLeft - mid;
+                card.parentElement.scrollBy({
+                    left: offSet,
+                    behavior: 'smooth'
+                });       
+            }
+        }, 170);
     });
 });
